9705d07e1f4f0910fd285c975337d3a3c1edeb10
Taras Rashkevych <taras.rashkevych1999@gmail.com>
2021-04-11 17:38:37 +0200

    add two python scripts for managing the files generated by latexmk

---
+++ scripts/moveTempFilesToSrc.py
@@ -0,0 +1,19 @@
+import glob
+import os
+
+PATH = 0
+FILE_NAME = 1
+
+if os.path.isdir("../tmp/"):
+    path_file_pairs = []
+    paths_to_files_in_tmp = glob.glob("../tmp/*")
+
+    for path in paths_to_files_in_tmp:
+        path_segments = path.split("/")
+        file_name = path_segments[len(path_segments) - 1]
+        path_file_pairs.append((path, file_name))
+
+    for path_file_pair in path_file_pairs:
+        os.rename(path_file_pair[PATH], "./" + path_file_pair[FILE_NAME])
+else:
+    os.mkdir("../tmp/")
---
+++ scripts/moveTempFilesToTmp.py
@@ -0,0 +1,18 @@
+import glob
+import os
+
+PATH = 0
+FILE_NAME = 1
+
+path_file_pairs = []
+files_to_exclude = ["notes.pdf", "notes.tex"]
+paths_to_files_in_src = glob.glob("../src/*.*")
+
+for path in paths_to_files_in_src:
+    path_segments = path.split("/")
+    file_name = path_segments[len(path_segments) - 1]
+    if file_name not in files_to_exclude:
+        path_file_pairs.append((path, file_name))
+
+for path_file_pair in path_file_pairs:
+    os.rename(path_file_pair[PATH], "../tmp/" + path_file_pair[FILE_NAME])
